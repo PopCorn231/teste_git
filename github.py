c8aabab23da5a13a214396e9ac80f7bbed4610a1
Diego <dmriani.15@gmail.com>
2025-03-26 11:09:25 -0300

    Criação do arquivo python.

---
+++ github.py
@@ -0,0 +1,11 @@
+def calcular_media(n1, n2, n3, n4):
+    total = n1 + n2 + n3 + n4
+    media = total/4
+    return media
+
+n1 = int(input("Digite sua nota: "))
+n2 = int(input("Digite sua nota: "))
+n3 = int(input("Digite sua nota: "))
+n4 = int(input("Digite sua nota: "))
+
+calcular_media(n1, n2, n3, n4)
